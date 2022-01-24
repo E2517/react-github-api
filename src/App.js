b6d7d4f4f8870f5cb20bd944c674c1da90eaaecf
e2517 <e2517dev@gmail.com>
2022-01-24 19:53:09 +0000

    React API

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const gitHubUrl = "https://api.github.com/users/e2517";
+
+function App() {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    getGitHubUserWithFetch();
+  }, []);
+
+  const getGitHubUserWithFetch = async () => {
+    const response = await fetch(gitHubUrl);
+    const jsonData = await response.json();
+    setUserData(jsonData);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>GitHub User Data</h2>
+        <div className="user-container">
+          <h5 className="info-item">{userData.name}</h5>
+          <h5 className="info-item">{userData.location}</h5>
+          <h5 className="info-item">{userData.blog}</h5>
+          <h5 className="info-item">{userData.company}</h5>
+        </div>
+      </header>
+    </div>
+  );
+}
+export default App;
