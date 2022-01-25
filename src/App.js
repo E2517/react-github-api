c1d537e3c3316f597dd26288e268f1a72ca70625
e2517 <e2517dev@gmail.com>
2022-01-25 08:30:59 +0000

    API

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import "./App.css";
 
 const gitHubUrl = "https://api.github.com/users/e2517";
 
+const gitHubUrlRepos = "https://api.github.com/users/e2517/repos";
+
 function App() {
   const [userData, setUserData] = useState({});
+  const [userDataRepos, setUserDataRepos] = useState([]);
 
   useEffect(() => {
     getGitHubUserWithFetch();
+    getGitHubReposWithFetch();
   }, []);
 
   const getGitHubUserWithFetch = async () => {
     const response = await fetch(gitHubUrl);
     const jsonData = await response.json();
     setUserData(jsonData);
+  };
+
+  const getGitHubReposWithFetch = async () => {
+    const response = await fetch(gitHubUrlRepos);
+    const jsonData = await response.json();
+    setUserDataRepos(jsonData);
   };
 
   return (
@@ -25,6 +35,13 @@ function App() {
           <h5 className="info-item">{userData.location}</h5>
           <h5 className="info-item">{userData.blog}</h5>
           <h5 className="info-item">{userData.company}</h5>
+        </div>
+        <div>
+          <ul>
+            {userDataRepos.map(item => (
+              <li key={item.id}>${item.name}</li>
+            ))}
+          </ul>
         </div>
       </header>
     </div>
